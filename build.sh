5e39de870a34079efbf8d228c60539e69d3b7ada
Sanjan Grero <sgrero@salesforce.com>
2023-07-29 01:06:55 +0930

    readding requirements.txt

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 python -m pip install --upgrade pip
 
-pip install pipenv
-pipenv install
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
